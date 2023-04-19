a35fbd57178c0a5b44614d35dd9343c925c3ba69
Doniyorbek <qahramonovdoniyor05@gmail.com>
2023-04-19 14:19:32 +0300

    doner

---
+++ Yoshni_aniqlash/main.py
@@ -0,0 +1,55 @@
+# FULL_NAME = input("Ism familiya: ")
+# YEAR_OF_BIRTH = input("Tug'ilgan yil: ")
+
+# YEAR_OF_BIRTH = int(YEAR_OF_BIRTH)
+
+# CURRENT_YEAR = 2023
+
+# age = CURRENT_YEAR - YEAR_OF_BIRTH
+
+# natija = f"{FULL_NAME} {age} yoshda!"
+
+# print(natija)
+
+#---------------------------------------------------
+# Sinfga qarab yoshni aniqlash formulasi
+
+# FULL_NAME = input("Ism familiya: ")
+# CLASS = input("Sinfingizni kiriting: ")
+
+# CLASS = int(CLASS)
+
+# age = CLASS + 7
+
+# natija = f"{FULL_NAME} {age} yoshda!"
+
+# print(natija)
+
+#-----------------------------------------------------
+# Tug'ligan yilga qarab sinfni aniqlash dasturi
+
+
+# FULL_NAME = input("Ism familiya: ")
+# CLASS = input("Yoshingizni kiriting: ")
+
+# CLASS = int(CLASS)
+
+# age = CLASS - 7
+
+# natija = f"{FULL_NAME} {age} sinfsiz!"
+
+# print(natija)
+
+#-------------------------------------------------------
+# Sinfga qarab tug'ilgan yilni aniqlash dasturi
+
+FULL_NAME = input("Ism familiya: ")
+CLASS = input("Sinfingizni kiriting: ")
+
+CLASS = int(CLASS)
+
+age = CLASS - 7
+
+natija = f"{FULL_NAME} {age} yilsiz!"
+
+print(natija)
--- main.py
+++ main.py
@@ -2,6 +2,16 @@ ism = input("Ismingiz nima?:")
 
 familiya = input("Familiyangiz nima?:")
 
-natija = "Salom" + "" + ism + "" + familiya
+natija = "Salom "+ ism + " " + familiya
+
+natija = f"Salom {ism} {familiya}"
 
 print(natija)
+
+# foo = "Laa"
+# baz = "Bla"
+# qwe = -5
+
+# turi = type(qwe)
+
+# print(turi)
