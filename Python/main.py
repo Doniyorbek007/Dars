961e3785b0dc11df9dd37c8e4c9c4b98b98bbc54
Doniyorbek <qahramonovdoniyor05@gmail.com>
2023-04-26 13:53:59 +0300

    !!!

---
+++ Python/main.py
@@ -0,0 +1,23 @@
+# text = "Salom" #input("So'z kirting: ")
+
+# soni = len(text)
+
+# # index -> min = 0, max = len() - 1
+# print(text[1])
+
+#---------------------------------------------------
+
+# String ni teskari chiqarish
+
+# text =input("So'z kirting: ")  #"Salom" 
+
+# soni = len(text)
+
+# # index -> min = 0, max = len() - 1
+# print(text[::-1])
+
+#----------------------------------------------------
+
+text = "Salom barchaga!"
+
+print(text)
