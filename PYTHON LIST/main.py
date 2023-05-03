f69dc136f63dc1222d0116fa40a8990e62f0a3a0
Doniyorbek <qahramonovdoniyor05@gmail.com>
2023-05-03 14:06:01 +0300

    ...

--- PYTHON LIST/main.py
+++ PYTHON LIST/main.py
@@ -10,16 +10,29 @@
 # aralash_savat = ["Olma", 12, "Non", True, -5, 0.2]
 # print(aralash_savat[3]) # True
 
-mevalar = ["2kg Olma", "2kg Nok"]
+# mevalar = ["2kg Olma", "2kg Nok"]
 
-mevalar[0] = "2 ta Banan"
-mevalar.append("2 ta Apelsin")
-mevalar.append("4 ta kevi")
-mevalar.remove("2kg Nok")
-nimani_chopding = mevalar.pop(2)
-mevalar.append(nimani_chopding)
-mevalar.insert(2, "5 ta Nok")
-del mevalar[0]
+# mevalar[0] = "2 ta Banan"
+# mevalar.append("2 ta Apelsin")
+# mevalar.append("4 ta kevi")
+# mevalar.remove("2kg Nok")
+# nimani_chopding = mevalar.pop(2)
+# mevalar.append(nimani_chopding)
+# mevalar.insert(2, "5 ta Nok")
+# del mevalar[0]
 
-print(mevalar)
-# print(nimani_chopding)
+# print(mevalar)
+# # print(nimani_chopding)
+
+# rgb = ('red', 'green', 'blue')
+#   # O'zgartirib bo'lmaydi
+
+# print(rgb[0])
+# print(rgb[1])
+# print(rgb[2])
+
+# mevalar = ["Nok ", "Olma ", "Banan "]
+
+# miqdori = ['1 kg', '2 ta', '5 ta'] 
+
+# mevalar_miqdori = ["Nok 1kg", "Olma 2 ta", "Banan 5 ta"]
