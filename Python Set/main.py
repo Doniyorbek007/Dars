4b4d5d22bbea210927b8caee71e55021cef93147
Doniyorbek <qahramonovdoniyor05@gmail.com>
2023-05-10 13:35:45 +0300

    git add .!

---
+++ Python Set/main.py
@@ -0,0 +1,66 @@
+# my_dict = {}
+
+# my_set = set()
+
+# print(type(my_dict))
+# print(type(my_set))
+
+# #------------------------------
+
+# kasblar = {'Dasturchi','Dezayner','Loyiha menejeri'}
+
+# kasblar.add('Testlovchi')
+# kasblar.remove('DevOps') # KeyError: 'DevOps'
+# kasblar.discard('DevOps') # Error not found!
+# kasblar.discard('Loyiha menejeri')
+
+# chopilgan_kasb = kasblar.pop()
+
+# kasblar.clear()
+
+
+# print(chopilgan_kasb)
+
+# print(kasblar)
+ 
+# #------------------------------
+
+# a = 10
+# b = 15
+
+# if not a < b:
+#     print("Ishladi")
+# else:
+#     print("Ishlamadi")
+
+# if a != b:
+#     print("Ishladi")
+# else:
+#     print("Ishlamadi")
+
+# #------------------------------
+
+# skills = {'Problem solving', 'Software design', 'Python programming'}
+
+# for index, skills in enumerate(skills):
+#     print(f"{index}.{skills}")
+
+# print(enumarate(skills)) # <enumarete objects at 0x00000239AA6A5350>
+
+# #------------------------------
+
+# skills = {'Problem solving', 'Software design', 'Python programming'}
+
+# for index, skills in enumerate(skills, 1):
+#     print(f"{index}.{skills}")
+
+# #------------------------------
+
+# tags = {'Django', 'Pandas', 'Numpy'}
+# new_tags = {tag.lower() for tag in tags if tag != 'Numpy'}
+
+# print(new_tags)
+
+# print(set(tag.lower() for tag in tags if tag != 'Numpy'))
+
+# #------------------------------
