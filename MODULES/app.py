8ac2987e097fbe12ec6d054b3920e670cbf032f6
Doniyorbek <qahramonovdoniyor05@gmail.com>
2023-05-12 13:43:39 +0300

    t

---
+++ MODULES/app.py
@@ -0,0 +1,10 @@
+# def salomlash(ismi):
+#     return f"Salom {ismi}"
+
+#------------------------------------
+def salomlash_kim_bilandir(ismi):
+    return f"Salom {ismi}"
+
+
+def sum(a,b):
+    return a + b
---
+++ MODULES/main.py
@@ -0,0 +1,15 @@
+# import app # X
+
+# from app import salomlash
+
+# print(salomlash("Sardor"))
+# print(sum(15,20))
+
+#-----------------------------------------
+
+from app import salomlash_kim_bilandir as salom
+
+print(salom("Sardor"))
+
+#-----------------------------------------
+
