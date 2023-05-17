3a6d0dc95689095091c82669d289dfe72d2d731f
Doniyorbek <qahramonovdoniyor05@gmail.com>
2023-05-17 13:39:25 +0300

    ...

--- FILES/main.py
+++ FILES/main.py
@@ -40,12 +40,30 @@
 
 #---------------------------------------
 
-login = input('Login: ')
-parol = input('Parol: ')
+# login = input('Login: ')
+# parol = input('Parol: ')
 
-with open('./demo.txt') as f:
-    user_data = (f.readline()[:-1], f.readline())
-    if login == user_data[0] and parol == user_data[1]:
-        print('Wellcome!')
-    else:
-        print('Uyga bor!')
+# with open('./demo.txt') as f:
+#     user_data = (f.readline()[:-1], f.readline())
+#     if login == user_data[0] and parol == user_data[1]:
+#         print('Wellcome!')
+#     else:
+#         print('Uyga bor!')
+
+#---------------------------------------
+
+words_count = int(input("Words count: "))
+
+count = 1 
+while count <= words_count :
+    word = input(f"{count} Word: ")
+    with open("./app.txt", "at") as f:
+        f.write(word)
+        f.write("\n")
+
+    count += 1
+    
+with open("./app.txt") as f:
+    result = f.read()
+    print(result)
+        
