28b63a900380b06067b0621bc93dcb4298d4b64c
Doniyorbek <qahramonovdoniyor05@gmail.com>
2023-05-31 18:39:32 +0300

    ...

--- Python Telegram Bots/main.py
+++ Python Telegram Bots/main.py
@@ -1,8 +1,26 @@
-import telegram
+# import telegram
 
-print(telegram.__bot_api_version__) # 6.7
-print(telegram.__bot_api_version_info__) # 6.7
+# print(telegram.__bot_api_version__) # 6.7
+# print(telegram.__bot_api_version_info__) # 6.7
 
-print(telegram.__version__) # 20.3
-print(telegram.__version_info__) # 20.3
+# print(telegram.__version__) # 20.3
+# print(telegram.__version_info__) # 20.3
 
+
+try:
+    son = 10
+    print(son / 0)
+except ZeroDivisionError:
+    print("0 ga bo'lib bo'lmaydi")
+
+except TypeError:
+    print("Qiymatda xatolik bor")
+
+except NameError:
+    print("Nomida xatp bor")
+
+except SyntaxError as err:
+    print(err)
+
+finally:
+    print('Finishing up.')
