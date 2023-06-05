4304083347556ff0edccce63ae94ce76fcd315f9
Doniyorbek <qahramonovdoniyor05@gmail.com>
2023-06-05 16:26:30 +0300

    ...

--- JSON/main.py
+++ JSON/main.py
@@ -1,3 +1,9 @@
 import json
 
-# dump dumps load loads
+data = {
+    "data_name": "Data Name",
+    "data_info": "Data Info",
+    "data_date": 2023,
+    "is_active": True,
+    "data_types": ["data_1", ("data_2","data_3")]
+}
