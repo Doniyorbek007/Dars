82bdd409c92eb9d316797adb2ec8be5b8cca34de
Doniyorbek <qahramonovdoniyor05@gmail.com>
2023-06-09 17:19:27 +0300

    ...

--- JSON/script.js
+++ JSON/script.js
@@ -1,14 +1,5 @@
 const root = document.getElementById("root");
 
-// const ishchi = {
-//     ishchi_nomi: "Jhon Doe",
-//     ish_joyi: "Hokimiyat",
-//     ish_vaqti: "9:00/18:00",
-//     daromadi: 2000000
-// } 
-
-// console.log(ishchi.ish_joyi);
-
 fetch("data.json")
     .then(response => response.json())
     .then(data => {
